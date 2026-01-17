6a6fb940110b2713d01c76bf41ee6095d03c3644
Antoine <saruelucas2@gmail.com>
2026-01-17 14:28:14 +0100

    clean code (not finished jwt)

--- app/otp/page.tsx
+++ app/otp/page.tsx
@@ -1,10 +1,13 @@
 import { OTPForm } from "@/components/otp-form"
+import { Suspense } from "react"
 
 export default function OTPPage() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-xs">
-        <OTPForm />
+        <Suspense fallback={<div className="text-center">Chargement...</div>}>
+          <OTPForm />
+        </Suspense>
       </div>
     </div>
   )

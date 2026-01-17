569d25fd1e5564cf662f67c35dfdf2c1f0a0aa56
Mlax22 <165811882+Mlax22@users.noreply.github.com>
2026-01-17 16:06:41 +0100

    Revert "fix complete"

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { redirect } from "next/navigation";
 import Image from "next/image";
 
 export default function Home() {
-    redirect("/landing-page") 
-
   return (
   <></>
-  )   ;
+  );
 }
